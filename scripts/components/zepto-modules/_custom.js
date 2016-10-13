11bac84ae3435007f9000bd89250da7d61399aaf
gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/ <gem install sass>
2016-10-13 20:57:27 +0800

    no.1

---
+++ scripts/components/zepto-modules/_custom.js
@@ -0,0 +1,12 @@
+var $ = require('./zepto');
+
+require('./event');
+require('./form');
+require('./ie');
+require('./detect');
+require('./data');
+require('./touch');
+require('./stack');
+require('./ajax');
+
+module.exports = $;
